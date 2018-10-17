e5a2a612697da4c14a76a8767c93987a1097ca42
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-17 04:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine (katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'The line is currently empty.'
   } else {
-  var string = ["The line is currently: ", []]
+  var string = "The line is currently: ", []
   for (i = 0; i < katzDeliLine.length; i++) {
     string.push(`${i + 1} katzDeliLine[${i}],`);
   };
