69851b94956e3e3a07824dff27496583a681a8c9
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-17 05:33:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine (katzDeliLine) {
     return 'The line is currently empty.'
   } else {
   for (var i = 0; i < katzDeliLine.length; i++) {
-    string.push(`${i + 1}. ${katzDeliLine[i]}`);
+    string.push[0](`${i + 1}. ${katzDeliLine[i]}`);
   };
   return string;
 };
