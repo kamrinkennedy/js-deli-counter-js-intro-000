e57446d60a04e9d2054b88aab0e980c4c7dc5ea9
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-17 06:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine (katzDeliLine) {
   } else {
   var startingMessage = 'The line is currently: '
   for (var i = 0; i < katzDeliLine.length; i++) {
-    startingMessage +=`${i + 1}. ${katzDeliLine[i]}, `;
+    startingMessage +=`${i + 1}. ${katzDeliLine[i]} `;
   }
   // else {
   //   startingMessage += ` ${i + 1}. ${katzDeliLine[i]}`
